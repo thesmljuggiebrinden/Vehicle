368ec2684cb4a6e48cd11c5255155185fe578456
brind <bbhattacharya@student.bridgew.edu>
2022-12-01 10:23:12 -0500

    First commit

--- vehicles.py
+++ vehicles.py
@@ -1,10 +1,45 @@
+from typing import Tuple
+
+
 class Vehicles:
-    def __init__(self, make, model):
+    def __init__(self, make, model, length, width, height, wheelbase, weight,
+                 max_load, cargo_volume, towing_capacity, front_shoulderroom,
+                 front_legroom, front_headroom, rear_shoulderroom, rear_legroom,
+                 rear_headroom, thirdrow_shoulderroom, thirdrow_legroom, thirdrow_headroom):
         self.make = make
         self.model = model
+        self.length = length
+        self.width = width
+        self.height = height
+        self.wheelbase = wheelbase
+        self.weight = weight
+        self.max_load = max_load
+        self.cargo_volume = cargo_volume
+        self.towing_capacity = towing_capacity
+        self.front_shoulderroom = front_shoulderroom
+        self.front_legroom = front_legroom
+        self.front_headroom = front_headroom
+        self.rear_shoulderroom = rear_shoulderroom
+        self.rear_legroom = rear_legroom
+        self.rear_headroom = rear_headroom
+        self.thirdrow_shoulderroom = thirdrow_shoulderroom
+        self.thirdrow_legroom = thirdrow_legroom
+        self.thirdrow_headroom = thirdrow_headroom
 
-    def describe(self):
+
+    def details(self):
         print("{} {}".format(self.make, self.model))
+        print("Exterior Dimensions + Weight :{} in. {} in. {} in. {} in. {} lb.".format
+              (self.length, self.width, self.height,self.wheelbase, self.weight))
+        print("Cargo: {} lb. {} cu.Ft {} lb.".format
+              (self.max_load, self.cargo_volume, self.towing_capacity))
+        print("Interior Dimensions: "
+              "{} in. {} in. {} in. {} in. {} in. {} in. {} in. {} in. {} in.".format
+              (self.front_shoulderroom, self.front_legroom, self.front_headroom,
+               self.rear_shoulderroom, self.rear_legroom, self.rear_headroom,
+               self.thirdrow_shoulderroom, self.thirdrow_legroom, self.thirdrow_headroom))
+
+
 
 class Smart(Vehicles):
     pass
@@ -87,9 +122,11 @@ class Volkswagen(Vehicles):
 class Volvo(Vehicles):
     pass
 
-smart = Smart('Smart', 'EQ Fortwo')
+smart = Smart('Smart', 'EQ Fortwo', 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+smart.details()
 
-smart.describe()
+
+
 
 
 
